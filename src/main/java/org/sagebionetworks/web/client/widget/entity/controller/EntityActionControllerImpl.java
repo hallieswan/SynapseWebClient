9ee278998f81a9006ff7ff75c6d9000b5ff1f601
Jay Hodgson <jay.hodgson@sagebase.org>
2024-11-19 09:48:29 -0800

    SWC-7173

--- src/main/java/org/sagebionetworks/web/client/widget/entity/controller/EntityActionControllerImpl.java
+++ src/main/java/org/sagebionetworks/web/client/widget/entity/controller/EntityActionControllerImpl.java
@@ -712,34 +712,9 @@ public class EntityActionControllerImpl
     actionMenu.setActionListener(
       Action.REPORT_VIOLATION,
       (action, event) -> {
-        // report abuse via Jira issue collector
-        String userId = WebConstants.ANONYMOUS, email =
-          WebConstants.ANONYMOUS, displayName = WebConstants.ANONYMOUS, synId =
-          entity.getId();
-        UserProfile userProfile =
-          authenticationController.getCurrentUserProfile();
-        if (userProfile != null) {
-          userId = userProfile.getOwnerId();
-          displayName = DisplayUtils.getDisplayName(userProfile);
-          email = DisplayUtils.getPrimaryEmail(userProfile);
-        }
-
-        ginInjector
-          .getSynapseJSNIUtils()
-          .showJiraIssueCollector(
-            "", // summary
-            FLAG_ISSUE_DESCRIPTION_PART_1 +
-            gwt.getCurrentURL() +
-            FLAG_ISSUE_DESCRIPTION_PART_2,
-            FLAG_ISSUE_COLLECTOR_URL,
-            userId,
-            displayName,
-            email,
-            synId, // Synapse data object ID
-            REVIEW_DATA_REQUEST_COMPONENT_ID,
-            null, // AR ID
-            FLAG_ISSUE_PRIORITY
-          );
+        popupUtils.openInNewWindow(
+          "https://sagebionetworks.jira.com/servicedesk/customer/portal/20"
+        );
       }
     );
   }
